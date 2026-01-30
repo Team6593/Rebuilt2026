e99d547a5193fd9ff361a57c9d0977c06915a2b1
StudioNafi <nafijahan08@gmail.com>
2026-01-29 22:33:32 -0500

    Simulated RPM control.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import com.ctre.phoenix6.HootAutoReplay;
 
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.simulation.BatterySim;
@@ -15,6 +16,10 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.simulation.commands.ShooterSimulationRPMCommand;
+import frc.robot.simulation.commands.ShooterSimulationShoot;
+import frc.robot.simulation.shooter.ShooterSimulation;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
@@ -22,6 +27,10 @@ public class Robot extends TimedRobot {
     private Field2d field = new Field2d();
 
     private final RobotContainer m_robotContainer;
+
+    private ShooterSimulation shooterSimulation = new ShooterSimulation();
+
+    private CommandXboxController simJoystick = new CommandXboxController(3);
 
     /* log and replay timestamp and joystick data */
     private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay()
@@ -112,5 +121,10 @@ public class Robot extends TimedRobot {
     public void testExit() {}
 
     @Override
-    public void simulationPeriodic() {}
+    public void simulationPeriodic() {
+
+        simJoystick.x().whileTrue(new ShooterSimulationRPMCommand(shooterSimulation));
+        simJoystick.a().whileTrue(new ShooterSimulationShoot(shooterSimulation));
+
+    }
 }
---
+++ src/main/java/frc/robot/simulation/shooter/ShooterSimulationConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.simulation.shooter;
+
+public interface ShooterSimulationConstants {
+    int shooterID = 32;
+    int indexerID = 33;
+    double shooterRatio = 1;
+}
